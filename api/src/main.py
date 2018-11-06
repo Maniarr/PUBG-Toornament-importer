8304415e5841dc09c1a82ad4b4e75640e1c95647
Maniarr <nicolas@maniarr.fr>
2018-11-06 16:08:52 +0100

    change authentication method

--- api/src/main.py
+++ api/src/main.py
@@ -24,35 +24,52 @@ pubg_api = pubg.Api(
     os.getenv("PUBG_API_KEY")
 )
 
-@app.route("/")
-def home():
-    if "toornament_token" in session:
-        return "access: {}".format(session["toornament_token"]["access_token"])
+def create_json_response(data, status_code):
+    return app.response_class(
+        response=json.dumps(data),
+        status=status_code,
+        mimetype='application/json'
+    )
 
+@app.route("/login", methods=["GET"])
+def home():
     csrf_token = str(uuid.uuid4())
 
     session["csrf_token"] = csrf_token
 
-    return "<a href=\"https://account.toornament.com/oauth2/authorize?response_type=code&client_id={}&redirect_uri={}&scope={}&state={}\">Login with Toornament</a>".format(
-        os.getenv("TOORNAMENT_CLIENT_ID"),
-        os.getenv("REDIRECT_URI"),
-        "organizer:view organizer:result",
-        csrf_token
-    )
+    return create_json_response({
+        "connection_uri": "https://account.toornament.com/oauth2/authorize?response_type=code&client_id={}&redirect_uri={}&scope={}&state={}".format(
+            os.getenv("TOORNAMENT_CLIENT_ID"),
+            os.getenv("REDIRECT_URI"),
+            "organizer:view organizer:result",
+            csrf_token
+        )
+    }, 200)
 
-@app.route("/login")
+@app.route("/login", methods=["POST"])
 def login():
-    code = request.args.get("code") 
-    print(code)
+    if "code" not in request.json:
+        return create_json_response({
+            "error": "No code provided"
+        }, 401)
 
-    if code is None:
-        return "No code provided by Toornament"
-    
+    if "state" not in request.json:
+        return create_json_response({
+            "error": "No state provided"
+        }, 401)
+
     if "csrf_token" not in session:
-        return redirect("/", code=302)
+        return create_json_response({
+            "error": "No CSRF provided"
+        }, 401)
 
-    if request.args.get("state") != session["csrf_token"]:
-        return "CSRF token not valid"
+    code = request.json["code"]
+    state = request.json["state"]
+
+    if state != session["csrf_token"]:
+        return  create_json_response({
+            "error": "State/CSRF token not valid"
+        }, 401)
 
     session.pop("csrf_token")
 
@@ -60,59 +77,66 @@ def login():
 
     token = api_user.get_tokens(code)
 
-    if token is not None:
-        session["toornament_token"] = token
+    if token is None:
+        return create_json_response({
+            "error": "retry"
+        }, 401)
 
-    return redirect("/", code=302)
+    return create_json_response(token, 200)
 
-@app.route("/tournaments")
+@app.route("/tournaments", methods=["GET"])
 def tournaments():
-    if "toornament_token" not in session:
-        return redirect("/", code=302)
+    if "Authorization" not in request.headers:
+        return create_json_response({
+            "error": "No \"Authorization\" header provided"
+        }, 401)
 
     api_user = toornament.ApiUser(toornament_api)
-    api_user.access_token = session["toornament_token"]["access_token"]
+    api_user.access_token = request.headers["Authorization"]
 
-    return json.dumps(api_user.get_tournaments())
+    return create_json_response(api_user.get_tournaments(), 200)
 
-
-@app.route("/tournaments/<tournament_id>/matches")
+@app.route("/tournaments/<tournament_id>/matches", methods=["GET"])
 def tournament(tournament_id):
-    if "toornament_token" not in session:
-        return redirect("/", code=302)
+    if "Authorization" not in request.headers:
+        return create_json_response({
+            "error": "No \"Authorization\" header provided"
+        }, 401)
 
     api_user = toornament.ApiUser(toornament_api)
-    api_user.access_token = session["toornament_token"]["access_token"]
+    api_user.access_token = request.headers["Authorization"]
 
-    return json.dumps(api_user.get_matches(tournament_id))
+    return create_json_response(api_user.get_matches(tournament_id), 200)
 
-@app.route("/tournaments/<tournament_id>/matches/<match_id>")
+@app.route("/tournaments/<tournament_id>/matches/<match_id>", methods=["GET"])
 def games(tournament_id, match_id):
-    if "toornament_token" not in session:
-        return redirect("/", code=302)
+    if "Authorization" not in request.headers:
+        return create_json_response({
+            "error": "No \"Authorization\" header provided"
+        }, 401)
 
     api_user = toornament.ApiUser(toornament_api)
-    api_user.access_token = session["toornament_token"]["access_token"]
+    api_user.access_token = request.headers["Authorization"]
 
-    return json.dumps(api_user.get_games(tournament_id, match_id))
+    return create_json_response(api_user.get_games(tournament_id, match_id), 200)
 
-@app.route("/pubg/tournaments")
+@app.route("/pubg/tournaments", methods=["GET"])
 def pubg_tournaments():
-    if "toornament_token" not in session:
-        return redirect("/", code=302)
+    if "Authorization" not in request.headers:
+        return create_json_response({
+            "error": "No \"Authorization\" header provided"
+        }, 401)
 
-    print(pubg_api.api_key)
+    return create_json_response(pubg_api.get_tournaments(), 200)
 
-    return json.dumps(pubg_api.get_tournaments())
-
-@app.route("/pubg/tournaments/<tournament_id>")
+@app.route("/pubg/tournaments/<tournament_id>", methods=["GET"])
 def pubg_tournament(tournament_id):
-    if "toornament_token" not in session:
-        return redirect("/", code=302)
+    if "Authorization" not in request.headers:
+        return create_json_response({
+            "error": "No \"Authorization\" header provided"
+        }, 401)
 
-    print(pubg_api.api_key)
-
-    return json.dumps(pubg_api.get_tournament(tournament_id))
+    return create_json_response(pubg_api.get_tournament(tournament_id), 200)
 
 if __name__ == "__main__":
     app.debug = True
